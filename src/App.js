3e9d65670f6534dedaa331d4be920a316d56351a
girkarkshitij <girkarkshitij@gmail.com>
2020-12-20 19:31:33 +0530

    Create basic structure of app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 class App extends React.Component {
     render() {
-        return <h1>Hello World</h1>;
+        return (
+            <div className='container'>
+                <h1>Markdown Previewer</h1>
+
+                <div className='editor-container'>
+                    <h2>Editor</h2>
+                    <div className='mark-input'>
+                        <textarea id='editor' cols='100' rows='20'></textarea>
+                    </div>
+                </div>
+
+                <div className='previewer-container'>
+                    <h2>Previewer</h2>
+                    <div id='preview'></div>
+                </div>
+            </div>
+        );
     }
 }
 
